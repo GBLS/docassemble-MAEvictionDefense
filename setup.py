47d6611ea53b3a4ea82ec9da1b92af3e2ecbabde
System Administrator <qsteenhuis@gmail.com>
2018-06-08 16:56:41 -0400

    Extensive reformatting of Answer and Discovery forms; some language cleanup for questions

--- setup.py
+++ setup.py
@@ -45,8 +45,10 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MAEvictionDefense',
-      version='0.0.30',
+      version='0.0.31',
       description=('A guided interview for pro se eviction defense in Massachusetts. Generates an Answer form, Request for Discovery, and accompanying forms and motions.'),
+      long_description="# docassemble.MAEvictionDefense\r\n\r\nA guided interview for pro se eviction defense in Massachusetts. Generates an Answer form, \r\nRequest for Discovery, and accompanying forms and motions.\r\n\r\n## Contributors:\r\n    \r\n1. Quinten Steenhuis, Esq. ([Greater Boston Legal Services](https://www.gbls.org))\r\n1. Rina Padua ([Greater Boston Legal Services](https://www.gbls.org) and Harvard University [Phillips Brooks House](http://pbha.org/))\r\n1. Caroline Robinson ([Massachusetts Law Reform Institute](http://www.mlri.org))\r\n1. Mariah Jennings-Rampsi, Esq. ([Volunteer Lawyer's Project](https://www.vlpnet.org/))\r\n",
+      long_description_content_type='text/markdown',
       author='Quinten Steenhuis, Greater Boston Legal Services',
       author_email='qsteenhuis@gbls.org',
       license='MIT',
@@ -54,7 +56,6 @@ setup(name='docassemble.MAEvictionDefense',
       packages=find_packages(),
       namespace_packages=['docassemble'],
       install_requires=[],
-      dependency_links=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MAEvictionDefense/', package='docassemble.MAEvictionDefense'),
      )
