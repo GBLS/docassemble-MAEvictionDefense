b8b3e8da5a65976ea96b49f29298c0f811fe2d2c
System Administrator <qsteenhuis@gmail.com>
2018-08-18 17:11:23 -0400

    Explain discovery to pro se users, review initial defenses, add detail to answer, allow counterclaims for offset to rent in fault case

--- setup.py
+++ setup.py
@@ -45,9 +45,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MAEvictionDefense',
-      version='0.0.43',
+      version='0.0.44',
       description=('A guided interview for pro se eviction defense in Massachusetts. Generates an Answer form, Request for Discovery, and accompanying forms and motions.'),
-      long_description="# docassemble.MAEvictionDefense\r\n\r\nA guided interview for pro se eviction defense in Massachusetts. Generates an Answer form, \r\nRequest for Discovery, and accompanying forms and motions.\r\n## Changelog\r\n2018-08-17 Email reminders, compact attachments page and wording / help improvements\r\n2018-07-08 Bug fixes\r\n2018-07-01 Added videos created by MLRI\r\n\r\n## Contributors:\r\n    \r\n1. Quinten Steenhuis, Esq. ([Greater Boston Legal Services](https://www.gbls.org))\r\n1. Rina Padua ([Greater Boston Legal Services](https://www.gbls.org) and Harvard University [Phillips Brooks House](http://pbha.org/))\r\n1. Caroline Robinson ([Massachusetts Law Reform Institute](http://www.mlri.org))\r\n1. Mariah Jennings-Rampsi, Esq. ([Volunteer Lawyer's Project](https://www.vlpnet.org/))\r\n",
+      long_description="# docassemble.MAEvictionDefense\r\n\r\nA guided interview for pro se eviction defense in Massachusetts. Generates an Answer form, \r\nRequest for Discovery, and accompanying forms and motions.\r\n## Changelog\r\n2018-08-18 Explain discovery to pro se users, review initial defenses, add detail to answer\r\n2018-08-17 Email reminders, compact attachments page and wording / help improvements\r\n2018-07-08 Bug fixes\r\n2018-07-01 Added videos created by MLRI\r\n\r\n## Contributors:\r\n    \r\n1. Quinten Steenhuis, Esq. ([Greater Boston Legal Services](https://www.gbls.org))\r\n1. Rina Padua ([Greater Boston Legal Services](https://www.gbls.org) and Harvard University [Phillips Brooks House](http://pbha.org/))\r\n1. Caroline Robinson ([Massachusetts Law Reform Institute](http://www.mlri.org))\r\n1. Mariah Jennings-Rampsi, Esq. ([Volunteer Lawyer's Project](https://www.vlpnet.org/))\r\n",
       long_description_content_type='text/markdown',
       author='Quinten Steenhuis, Greater Boston Legal Services',
       author_email='qsteenhuis@gbls.org',
